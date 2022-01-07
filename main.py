81198311cd3141490305ad1b33f9192d2d56149d
srkz <mbobletz@gmail.com>
2022-01-07 14:57:15 -0800

    for Heroku

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ import praw, yaml, time, traceback
 
 with open("config.yaml") as config_file:
     config = yaml.safe_load(config_file)
-    client_id = config["client_id"]
-    client_secret = config["client_secret"]
-    username = config["username"]
-    password = config["password"]
     user_agent = config["user_agent"]
     mod_sub = config["mod_sub"]
     subs = config["filtered_subs"]
 
-reddit = praw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     user_agent=user_agent,
-                     username=username,
-                     password=password)
+reddit = praw.Reddit(user_agent=user_agent)
 
 while True:
     try:
