3a4aad063bc29b2f15682d9a10972fd92197b8ec
srkz <mbobletz@gmail.com>
2022-11-20 21:18:16 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,29 +8,39 @@ with open("config.yaml") as config_file:
 
 reddit = praw.Reddit(user_agent=user_agent)
 
+streamComments = reddit.subreddit(mod_sub).stream.comments(pause_after=-1, skip_existing=True)
+
+print('About to start while loop!')
 while True:
     try:
-        for comment in reddit.subreddit(mod_sub).stream.comments(skip_existing=True):
-            print('new comment!')
-            is_filtered = False
-            author = comment.author.name
-            print('author:',author)
-            for acct_submission in reddit.redditor(author).submissions.new(limit=None):
-                if acc_submission.subreddit.display_name in subs:
-                    is_filtered = True
-                    print('filtered post in users history!')
-                    break
-            for acct_comment in reddit.redditor(author).comments.new(limit=None):
-                if acct_comment.subreddit.display_name in subs:
-                    is_filtered = True
-                    print('filtered comment in users history!')
-                    break
-            print('is filtered?',is_filtered)
-            if is_filtered == True:
-                print('removed comment')
-                comment.mod.remove()
+        for comment in streamComments:
+            if comment is None:
+                break
             else:
-                print('No filtered activity found.')
+                is_filtered = False
+                author = submission.author.name
+                
+                userSubmissions = reddit.redditor(author).submissions.new(limit=None)
+                for acct_submission in userSubmissions:
+                    if acct_submission.subreddit.display_name in subs:
+                        is_filtered = True
+                        break
+                    else:
+                        break
+                userComments = reddit.redditor(author).comments.new(limit=None)
+                for acct_comment in userComments:
+                    if acct_comment.subreddit.display_name in subs:
+                        is_filtered = True
+                        break
+                    else:
+                        break
+                if is_filtered == True:
+                    print('New comment from u/' + author + ': Removed due to filtered account activity.')
+                    comment.mod.remove()
+                    break
+                else:
+                    print('New comment from u/' + author + ': Clean account.')
+                    break
     except Exception:
         print(traceback.format_exc())
         time.sleep(60)
