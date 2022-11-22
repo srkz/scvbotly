9b060a53adacda0677633dd118c8cd597b71472d
srkz <mbobletz@gmail.com>
2022-11-22 12:34:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ while True:
                         break
                 if is_filtered == True:
                     print('New comment from u/' + author + ': Removed due to filtered account activity.')
-                    comment.mod.remove(mod_note='filtered activity')
+                    comment.mod.remove()
                     break
                 else:
                     print('New comment from u/' + author + ': Clean account.')
